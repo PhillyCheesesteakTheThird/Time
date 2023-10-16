46f6ee615381da5e58ebe5470971fae3646701a8
Jadan Kerr <badi1863@gmail.com>
2023-10-16 17:17:51 -0600

    did some things

---
+++ src/Time.java
@@ -0,0 +1,21 @@
+public class Time {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    public Time(){
+        this.hours = 12;
+        this.minutes = 0;
+        this.seconds = 0;
+    }
+    public Time(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+
+    }
+
+    public static Time fromString(String time) {
+        time.
+    }
+}
